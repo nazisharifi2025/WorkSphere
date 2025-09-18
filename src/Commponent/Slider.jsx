48b6a992cadfee82a40a7be4cf3815366bb44dc6
nazi sharifi <banonazisharifi@gmail.com>
2025-09-19 02:11:59 +0430

    style  added

--- src/Commponent/Slider.jsx
+++ src/Commponent/Slider.jsx
@@ -15,7 +15,7 @@ I'm always excited to connect with creative minds and explore new opportunities 
         </div>
         <div className="  lg:right-22 md:right-44 right-0 lg:bottom-27 bottom-14   absolute  rounded-full  lg:h-[414px] md:h-[414px] h-[410px] flex justify-center items-center lg:w-[414px] md:w-[414px] w-full mx-auto  bg-gradient-to-tr from-text-nav to-black animate-spin-slow">
           </div>    
-            <div className='lg:h-[400px] md:h-[400px] h-[396px] md:w-[400px] mx-auto w-[396px] lg:w-[400px] rounded-full bg-no-repeat text-nav bg-cover object-cover bg-center bg-[url(../public/Images/mee.jpg)]   '>
+            <div className='lg:h-[400px] md:h-[400px] h-[396px] md:w-[400px] mx-auto w-full lg:w-[400px] rounded-full bg-no-repeat text-nav bg-cover object-cover bg-center bg-[url(../public/Images/mee.jpg)]   '>
         </div>
           </div>
     </section>
